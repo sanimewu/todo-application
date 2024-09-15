0c0e19ef104c0bac0ca8c620ef81b2d8b82c8b6b
sanimewu <2020-1-60-267@std.ewubd.edu>
2024-09-15 13:22:03 +0600

    #7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,36 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject, OnDestroy, OnInit} from '@angular/core';
 import {UserService} from "./services/user.service";
 import {Router} from "@angular/router";
 import {DarkThemeService} from "./services/dark-theme.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'todo-application';
   isLoggedIn = false;
+  isDarkMode = false;
+  private subscription: Subscription = new Subscription();
   darkThemeService: DarkThemeService = inject(DarkThemeService);
+
   constructor() {
   }
   ngOnInit() {
-
+    this.subscription  = this.darkThemeService.darkMode$.subscribe(res=>{
+      this.isDarkMode = res;
+    })
+    console.log(this.darkThemeService.darkMode$);
   }
   login(){
+    this.isLoggedIn = !this.isLoggedIn;
   }
   toggleDarkMood(){
-    this.darkThemeService.updateDarkMood();
+    this.darkThemeService.toggleDarkMode();
+  }
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
--- src/app/services/dark-theme.service.ts
+++ src/app/services/dark-theme.service.ts
@@ -1,15 +1,23 @@
 import {Injectable, signal} from '@angular/core';
+import {BehaviorSubject, Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DarkThemeService {
-
-  darkMood = signal<string>('null');
+  private localStorageKey = 'darkMode';
+  private darkModeSubject = new BehaviorSubject<boolean>(this.getStoredDarkMode()) ;
+  darkMode$: Observable<boolean> = this.darkModeSubject.asObservable();
   constructor() { }
 
-  updateDarkMood(){
-    this.darkMood.update((res)=>(res==="dark"?"null":"dark"));
-    return this.darkMood;
+  toggleDarkMode() {
+    const newDarkMode = !this.darkModeSubject.value;
+    this.darkModeSubject.next(newDarkMode);//dynamically update the value
+    localStorage.setItem(this.localStorageKey, JSON.stringify(newDarkMode));
+  }
+
+  private getStoredDarkMode(): boolean {
+    const storedDarkMode = localStorage.getItem(this.localStorageKey);
+    return storedDarkMode !== null ? JSON.parse(storedDarkMode) : false;
   }
 }
